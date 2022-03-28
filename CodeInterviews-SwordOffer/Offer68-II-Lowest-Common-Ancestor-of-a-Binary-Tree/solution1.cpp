7514e5524b0f3eb00fa20c9e4105e9497557c274
Loyio <i@loyio.me>
2022-03-28 21:04:51 +0800

    add two binary tree problems solutions and dynamic programming problems

---
+++ CodeInterviews-SwordOffer/Offer68-II-Lowest-Common-Ancestor-of-a-Binary-Tree/solution1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* res;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q);
+        return res;
+    }
+
+    bool dfs(TreeNode *root, TreeNode* p, TreeNode* q){
+        if (root == nullptr){
+            return false;
+        }
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) || ((root->val == p->val || root->val == q->val) && (lson || rson))) {
+            res = root;
+        }
+        return lson || rson || (root->val == p->val || root->val == q->val);
+    }
+};
