371fa913c001d694f911ecd482240aed16832ebb
Loyio Hex <i@loyio.me>
2022-03-31 09:38:12 +0800

    just copy the hard problems code and kick two easy problems (#49)

---
+++ N1606-Find-Servers-That-Handled-Most-Number-of-Requests/solution1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> busiestServers(int k, vector<int> &arrival, vector<int> &load) {
+        set<int> available;
+        for (int i = 0; i < k; i++) {
+            available.insert(i);
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> busy;
+        vector<int> requests(k);
+        for (int i = 0; i < arrival.size(); i++) {
+            while (!busy.empty() && busy.top().first <= arrival[i]) {
+                available.insert(busy.top().second);
+                busy.pop();
+            }
+            if (available.empty()) {
+                continue;
+            }
+            auto p = available.lower_bound(i % k);
+            if (p == available.end()) {
+                p = available.begin();
+            }
+            requests[*p]++;
+            busy.emplace(arrival[i] + load[i], *p);
+            available.erase(p);
+        }
+        int maxRequest = *max_element(requests.begin(), requests.end());
+        vector<int> ret;
+        for (int i = 0; i < k; i++) {
+            if (requests[i] == maxRequest) {
+                ret.push_back(i);
+            }
+        }
+        return ret;
+    }
+};
