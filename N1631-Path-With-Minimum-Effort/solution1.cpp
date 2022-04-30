c4bac90c82edaaba293882a62075d85e4cf1d606
Loyio Hex <i@loyio.me>
2022-04-30 08:54:45 +0800

    add Path with minimun Effort UnionFind Solution (#79)

---
+++ N1631-Path-With-Minimum-Effort/solution1.cpp
@@ -0,0 +1,74 @@
+class UnionFind {
+public:
+    vector<int> parent;
+    vector<int> size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+    
+public:
+    UnionFind(int _n): n(_n), setCount(_n), parent(_n), size(_n, 1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int findset(int x) {
+        return parent[x] == x ? x : parent[x] = findset(parent[x]);
+    }
+    
+    bool unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            swap(x, y);
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+    
+    bool connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+};
+
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<tuple<int, int, int>> edges;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int id = i * n + j;
+                if (i > 0) {
+                    edges.emplace_back(id - n, id, abs(heights[i][j] - heights[i - 1][j]));
+                }
+                if (j > 0) {
+                    edges.emplace_back(id - 1, id, abs(heights[i][j] - heights[i][j - 1]));
+                }
+            }
+        }
+        sort(edges.begin(), edges.end(), [](const auto& e1, const auto& e2) {
+            auto&& [x1, y1, v1] = e1;
+            auto&& [x2, y2, v2] = e2;
+            return v1 < v2;
+        });
+
+        UnionFind uf(m * n);
+        int ans = 0;
+        for (const auto [x, y, v]: edges) {
+            uf.unite(x, y);
+            if (uf.connected(0, m * n - 1)) {
+                ans = v;
+                break;
+            }
+        }
+        return ans;
+    }
+};
