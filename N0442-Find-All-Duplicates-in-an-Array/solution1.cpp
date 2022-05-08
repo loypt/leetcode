ae695a8195477c9130d782a5de212db04a9d9498
Loyio <i@loyio.me>
2022-05-08 21:32:56 +0800

    add O(1) space complexity find duplicates in an array

---
+++ N0442-Find-All-Duplicates-in-an-Array/solution1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] - 1 != i) {
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
