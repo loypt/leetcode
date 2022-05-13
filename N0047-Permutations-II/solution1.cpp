c7f59427dfa561ca705a005c59419574ca8035a9
Loyio Hex <i@loyio.me>
2022-05-13 20:08:28 +0800

    add permutation II and delete columns count from fixed length nums (#92)

---
+++ N0047-Permutations-II/solution1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        helper(res, nums, 0);
+        return res;
+    }
+    
+    void helper(vector<vector<int>>& res, vector<int>& nums, int pos){
+        if(pos == nums.size()){
+            res.push_back(nums);
+        }else{
+            for(int i = pos; i < nums.size(); ++i){
+                if(i > pos && nums[i] == nums[pos]){
+                    continue;
+                }
+                swap(nums[pos], nums[i]);
+                helper(res, nums, pos+1);
+            }
+            for(int i = nums.size()-1; i > pos; --i){
+                swap(nums[pos], nums[i]);
+            }
+        }
+    }
+};
