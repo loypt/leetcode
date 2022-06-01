55aba1ef3aeac1dc5426893ce433ba90bd51bfd8
Loyio <i@loyio.me>
2022-06-01 20:33:38 +0800

    add matchsticks to square problem solution

---
+++ N0473-Matchsticks-to-Square/solution1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(int index, vector<int> &matchsticks, vector<int> &edges, int len) {
+        if (index == matchsticks.size()) {
+            return true;
+        }
+        for (int i = 0; i < edges.size(); i++) {
+            edges[i] += matchsticks[index];
+            if (edges[i] <= len && dfs(index + 1, matchsticks, edges, len)) {
+                return true;
+            }
+            edges[i] -= matchsticks[index];
+        }
+        return false;
+    }
+
+    bool makesquare(vector<int> &matchsticks) {
+        int totalLen = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        if (totalLen % 4 != 0) {
+            return false;
+        }
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>()); // 减少搜索量
+
+        vector<int> edges(4);
+        return dfs(0, matchsticks, edges, totalLen / 4);
+    }
+};
