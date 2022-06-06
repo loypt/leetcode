85fb03fc771ad310ebe68514dc16fe5335fc9b34
Loyio <i@loyio.me>
2022-06-06 20:55:20 +0800

    add my calendar problems solutions

---
+++ N0732-My-Calendar-III/solution1.cpp
@@ -0,0 +1,26 @@
+class MyCalendarThree {
+public:
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int start, int end) {
+        int ans = 0;
+        int maxBook = 0;
+        cnt[start]++;
+        cnt[end]--;
+        for (auto &[_, freq] : cnt) {
+            maxBook += freq;
+            ans = max(maxBook, ans);
+        }
+        return ans;
+    }
+private:
+    map<int, int> cnt;
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
