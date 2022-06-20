6346a93ebc74403fbdbffbae16cb3c233728fd85
Loyio <i@loyio.me>
2022-06-20 23:05:10 +0800

    add Range module and Insert Into a Sorted Circular Linked List solutions

---
+++ N0715-Range-Module/solution1.cpp
@@ -0,0 +1,76 @@
+class RangeModule {
+public:
+    RangeModule() {}
+    
+    void addRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                return;
+            }
+            if (start->second >= left) {
+                left = start->first;
+                intervals.erase(start);
+            }
+        }
+        while (it != intervals.end() && it->first <= right) {
+            right = max(right, it->second);
+            it = intervals.erase(it);
+        }
+        intervals[left] = right;
+    }
+    
+    bool queryRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it == intervals.begin()) {
+            return false;
+        }
+        it = prev(it);
+        return right <= it->second;
+    }
+    
+    void removeRange(int left, int right) {
+        auto it = intervals.upper_bound(left);
+        if (it != intervals.begin()) {
+            auto start = prev(it);
+            if (start->second >= right) {
+                int ri = start->second;
+                if (start->first == left) {
+                    intervals.erase(start);
+                }
+                else {
+                    start->second = left;
+                }
+                if (right != ri) {
+                    intervals[right] = ri;
+                }
+                return;
+            }
+            else if (start->second > left) {
+                start->second = left;
+            }
+        }
+        while (it != intervals.end() && it->first < right) {
+            if (it->second <= right) {
+                it = intervals.erase(it);
+            }
+            else {
+                intervals[right] = it->second;
+                intervals.erase(it);
+                break;
+            }
+        }
+    }
+
+private:
+    map<int, int> intervals;
+};
+
+/**
+ * Your RangeModule object will be instantiated and called as such:
+ * RangeModule* obj = new RangeModule();
+ * obj->addRange(left,right);
+ * bool param_2 = obj->queryRange(left,right);
+ * obj->removeRange(left,right);
+ */
---
+++ N0820-Short-Encoding-of-Words/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        unordered_set<string> good(words.begin(), words.end());
+        for (const string& word: words) {
+            for (int k = 1; k < word.size(); ++k) {
+                good.erase(word.substr(k));
+            }
+        }
+
+        int ans = 0;
+        for (const string& word: good) {
+            ans += word.size() + 1;
+        }
+        return ans;
+    }
+};
