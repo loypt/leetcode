8e67d531f8755bffcb57562c245da660fa5a28b3
Loyio <i@loyio.me>
2022-06-27 09:05:32 +0800

    add longest uncommon subsequence solutions

---
+++ N0522-Longest-Uncommon-Subsequence-II/solution1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLUSlength(vector<string>& strs) {
+        if (strs.empty()) return -1;
+        int rst = -1;
+        for(auto i = 0; i < strs.size(); ++i) {
+            int j = 0;
+            for(; j < strs.size(); ++j) {
+                if(i==j) continue;
+                if(LCS(strs[i], strs[j])) break;  // strs[j] is a subsequence of strs[i]
+            }
+            // strs[i] is not any subsequence of the other strings.
+            if(j==strs.size()) rst = max(rst, static_cast<int>(strs[i].size()));
+        }
+        return rst;
+    }
+
+    bool LCS(const string& a, const string& b) {
+        if (b.size() < a.size()) return false;
+        int i = 0;
+        for(auto ch: b) {
+            if(i < a.size() && a[i] == ch) i++;
+        }
+        return i == a.size();
+    }
+};
