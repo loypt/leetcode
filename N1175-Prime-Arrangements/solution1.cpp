7b86195f03d55084d70e7f769e2f1d4378731111
Loyio <i@loyio.me>
2022-06-30 20:42:21 +0800

    add prime arrangements problem solution

---
+++ N1175-Prime-Arrangements/solution1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    int MOD = 1e9 + 7;
+    
+public:
+    int numPrimeArrangements(int n) {
+        int numPrimes = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                numPrimes++;
+            }
+        }
+        return (int) (factorial(numPrimes) * factorial(n - numPrimes) % MOD);
+    }
+    
+    bool isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    long factorial(int n) {
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+            res %= MOD;
+        }
+        return res;
+    }
+};
