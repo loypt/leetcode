136d7da5a440a7cceff60be69da7f710cdf9bb5d
Loyio Hex <i@loyio.me>
2022-07-13 20:46:03 +0800

    add cells with odd values in a matrix solutions (#153)

---
+++ N1252-Cells-with-Odd-Values-in-a-Matrix/solution1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        vector<int> rows(m), cols(n);
+        for (auto & index : indices) {
+            rows[index[0]]++;
+            cols[index[1]]++;
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((rows[i] + cols[j]) & 1) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
