a2e0ca07ae35b0073638fd35c636cd922aecbf75
Loyio Hex <i@loyio.me>
2022-07-21 21:44:06 +0800

    Create solution1.cpp

---
+++ N0092-Reverse-Linked-List-II/solution1.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right) {
+        ListNode *dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+        ListNode *pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre->next;
+        }
+        ListNode *cur = pre->next;
+        ListNode *next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur->next;
+            cur->next = next->next;
+            next->next = pre->next;
+            pre->next = next;
+        }
+        return dummyNode->next;
+    }
+};
