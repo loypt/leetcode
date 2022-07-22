3e370da212e437e23a2b778e5da7705af9c23630
Loyio <i@loyio.me>
2022-07-22 22:55:42 +0800

    add partition list and set intersection size at least two

---
+++ N0086-Partition-List/solution1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small = new ListNode(0);
+        ListNode* smallHead = small;
+        ListNode* large = new ListNode(0);
+        ListNode* largeHead = large;
+        while (head != nullptr) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
