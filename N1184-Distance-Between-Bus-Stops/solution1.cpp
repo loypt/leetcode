c9955d9851f67a27d454eef44954ada231f90d9f
Loyio <i@loyio.me>
2022-07-24 21:08:36 +0800

    add distance between bus stops

---
+++ N1184-Distance-Between-Bus-Stops/solution1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+        if (start > destination) {
+            swap(start, destination);
+        }
+        return min(accumulate(distance.begin() + start, distance.begin() + destination, 0),
+                   accumulate(distance.begin(), distance.begin() + start, 0) +
+                   accumulate(distance.begin() + destination, distance.end(), 0));
+    }
+};
