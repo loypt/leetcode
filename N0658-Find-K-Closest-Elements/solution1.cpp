0e3ef078169bc50c1f896d4303c72798980426bf
Loyio Hex <i@loyio.me>
2022-08-26 22:38:14 +0800

    add find k closest elements solution (#194)

---
+++ N0658-Find-K-Closest-Elements/solution1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        sort(arr.begin(), arr.end(), [x](int a, int b) -> bool {
+            return abs(a - x) < abs(b - x) || abs(a - x) == abs(b - x) && a < b;
+        });
+        sort(arr.begin(), arr.begin() + k);
+        return vector<int>(arr.begin(), arr.begin() + k);
+    }
+};
