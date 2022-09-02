8fa28c2e9e3842d35ed54987ba8cae793df9f6f2
Loyio Hex <i@loyio.me>
2022-09-02 22:45:50 +0800

    add final prices with a special discount in a shop (#200)

---
+++ N1475-Final-Prices-With-a-Special-Discount-in-a-Shop/solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() > prices[i]) {
+                st.pop();
+            }
+            ans[i] = st.empty() ? prices[i] : prices[i] - st.top();
+            st.emplace(prices[i]);
+        }
+        return ans;
+    }
+};
