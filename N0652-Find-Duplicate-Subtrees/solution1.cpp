5cabb7ddc4f831ff2d26ef01818ed0f0eefdaf3a
Loyio Hex <i@loyio.me>
2022-09-06 21:36:57 +0800

    add find duplicate subtrees (#204)

---
+++ N0652-Find-Duplicate-Subtrees/solution1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        dfs(root);
+        return {repeat.begin(), repeat.end()};
+    }
+
+    string dfs(TreeNode* node) {
+        if (!node) {
+            return "";
+        }
+        string serial = to_string(node->val) + "(" + dfs(node->left) + ")(" + dfs(node->right) + ")";
+        if (auto it = seen.find(serial); it != seen.end()) {
+            repeat.insert(it->second);
+        }
+        else {
+            seen[serial] = node;
+        }
+        return serial;
+    }
+
+private:
+    unordered_map<string, TreeNode*> seen;
+    unordered_set<TreeNode*> repeat;
+};
