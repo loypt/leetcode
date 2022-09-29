74c538520716d1926a64b77df287ccd110548281
Loyio Hex <i@loyio.me>
2022-09-29 22:04:29 +0800

    add get kth magic number (#227)

---
+++ Interview/I17-09-Get-Kth-Magic-Number/solution1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getKthMagicNumber(int k) {
+        vector<int> dp(k + 1);
+        dp[1] = 1;
+        int p3 = 1, p5 = 1, p7 = 1;
+        for (int i = 2; i <= k; i++) {
+            int num3 = dp[p3] * 3, num5 = dp[p5] * 5, num7 = dp[p7] * 7;
+            dp[i] = min(min(num3, num5), num7);
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+            if (dp[i] == num7) {
+                p7++;
+            }
+        }
+        return dp[k];
+    }
+};
