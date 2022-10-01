8a463db55710dc449048c5173ea8da7e627c5345
Loyio Hex <i@loyio.me>
2022-10-01 17:11:09 +0800

    add zero matrix (#229)

---
+++ Interview/I01-08-Zero-Matrix/solution1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int flag_col0 = false;
+        for (int i = 0; i < m; i++) {
+            if (!matrix[i][0]) {
+                flag_col0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][j]) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (!matrix[i][0] || !matrix[0][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flag_col0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
