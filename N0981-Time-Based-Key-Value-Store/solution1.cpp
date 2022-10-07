15aeba05ccf7bdf510a8cf23c02adc7900d0d0a0
Loyio Hex <i@loyio.me>
2022-10-07 10:10:54 +0800

    add time base key value store (#235)

---
+++ N0981-Time-Based-Key-Value-Store/solution1.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].insert({ timestamp, value });
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = m[key].upper_bound(timestamp);
+        return it == m[key].begin() ? "" : prev(it)->second;
+    }
+private:
+    unordered_map<string, map<int, string>> m;
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
