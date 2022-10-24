ac501b978a3cb426cdd106ee074b363501375c6a
Loyio Hex <i@loyio.me>
2022-10-24 22:55:39 +0800

    add maxmum length of a concatenated string with unique characters

---
+++ N0915-Partition-Array-into-Disjoint-Intervals/solution1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> minRight(n);
+        minRight[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minRight[i] = min(nums[i], minRight[i + 1]);
+        }
+
+        int maxLeft = 0;
+        for (int i = 0; i < n - 1; i++) {
+            maxLeft = max(maxLeft, nums[i]);
+            if (maxLeft <= minRight[i + 1]) {
+                return i + 1;
+            }
+        }
+        return n - 1;
+    }
+};
