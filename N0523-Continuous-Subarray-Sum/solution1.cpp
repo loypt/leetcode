acde33908f558e37f3b7e1fee18bbb0e19e3d17a
Loyio Hex <i@loyio.me>
2022-10-26 22:10:36 +0800

    add continuous subarray sum

---
+++ N0523-Continuous-Subarray-Sum/solution1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int m = nums.size();
+        if (m < 2) {
+            return false;
+        }
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (mp.count(remainder)) {
+                int prevIndex = mp[remainder];
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                mp[remainder] = i;
+            }
+        }
+        return false;
+    }
+};
