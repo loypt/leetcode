cb71419e49f15fe48c97948c1fcf7de24bba9bb7
Loyio Hex <i@loyio.me>
2022-11-12 21:11:53 +0800

    add domino and trimino tiling

---
+++ N0790-Domino-and-Tromino-Tiling/solution1.cpp
@@ -0,0 +1,15 @@
+const long long mod = 1e9 + 7;
+class Solution {
+public:
+    int numTilings(int n) {
+        vector<vector<long long>> dp(n + 1, vector<long long>(4));
+        dp[0][3] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][3];
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            dp[i][3] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3]) % mod;
+        }
+        return dp[n][3];
+    }
+};
