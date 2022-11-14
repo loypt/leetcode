92dbc5cc11a186f3e906689be505e3cd4df679da
Loyio Hex <i@loyio.me>
2022-11-14 21:56:07 +0800

    add reverse words in a String (#270)

---
+++ N0791-Custom-Sort-String/solution1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> val(26);
+        for (int i = 0; i < order.size(); ++i) {
+            val[order[i] - 'a'] = i + 1;
+        }
+        sort(s.begin(), s.end(), [&](char c0, char c1) {
+            return val[c0 - 'a'] < val[c1 - 'a'];
+        });
+        return s;
+    }
+};
