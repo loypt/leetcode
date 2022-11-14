76dcf9f016c1a2794487493f9cf5118901731590
Loyio Hex <i@loyio.me>
2022-11-14 22:11:23 +0800

    add most stones removed with same row or column

---
+++ N0805-Split-Array-With-Same-Average/solution1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool splitArraySameAverage(vector<int> &nums) {
+        int n = nums.size(), m = n / 2;
+        if (n == 1) {
+            return false;
+        }
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        for (int &x : nums) {
+            x = x * n - sum;
+        }
+
+        unordered_set<int> left;
+        for (int i = 1; i < (1 << m); i++) {
+            int tot = 0;
+            for (int j = 0; j < m; j++) {
+                if (i & (1 << j)) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0) {
+                return true;
+            }
+            left.emplace(tot);
+        }
+
+        int rsum = accumulate(nums.begin() + m, nums.end(), 0);
+        for (int i = 1; i < (1 << (n - m)); i++) {
+            int tot = 0;
+            for (int j = m; j < n; j++) {
+                if (i & (1 << (j - m))) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0 || (rsum != tot && left.count(-tot))) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
