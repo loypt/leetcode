c5f05a2060bd4b7291b391c1dc05d80c312a7d02
Loyio Hex <i@loyio.me>
2022-11-17 22:21:44 +0800

    add guess number higher or lower

---
+++ N0775-Global-and-Local-Inversions/solution1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& nums) {
+        int n = nums.size(), minSuff = nums[n - 1];
+        for (int i = n - 3; i >= 0; i--) {
+            if (nums[i] > minSuff) {
+                return false;
+            }
+            minSuff = min(minSuff, nums[i + 1]);
+        }
+        return true;
+    }
+};
