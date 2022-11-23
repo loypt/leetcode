4bad97e0094c05305127c10ea92c2f939db16307
Loyio Hex <i@loyio.me>
2022-11-23 22:13:46 +0800

    add maximum number of balls in a box

---
+++ N1742-Maximum-Number-of-Balls-in-a-Box/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countBalls(int lowLimit, int highLimit) {
+        unordered_map<int, int> count;
+        int res = 0;
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int box = 0, x = i;
+            while (x) {
+                box += x % 10;
+                x /= 10;
+            }
+            count[box]++;
+            res = max(res, count[box]);
+        }
+        return res;
+    }
+};
