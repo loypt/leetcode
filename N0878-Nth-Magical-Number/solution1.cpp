7acb6b78cda7f4f503a631fb4add043d161a460c
Loyio Hex <i@loyio.me>
2022-11-23 21:55:25 +0800

    add nth magical number (#279)

---
+++ N0878-Nth-Magical-Number/solution1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int nthMagicalNumber(int n, int a, int b) {
+        long long l = min(a, b);
+        long long r = (long long) n * min(a, b);
+        int c = lcm(a, b);
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            long long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (r + 1) % MOD;
+    }
+};
