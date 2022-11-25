87fbf8ee0e144afa853f764285f9519ff7383162
Loyio Hex <i@loyio.me>
2022-11-25 21:25:51 +0800

    add number of subarrays with bounded maximum (#281)

---
+++ N0795-Number-of-Subarrays-with-Bounded-Maximum/solution1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int res = 0, last2 = -1, last1 = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= left && nums[i] <= right) {
+                last1 = i;
+            } else if (nums[i] > right) {
+                last2 = i;
+                last1 = -1;
+            }
+            if (last1 != -1) {
+                res += last1 - last2;
+            }
+        }
+        return res;
+    }
+};
