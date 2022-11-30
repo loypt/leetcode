3fef19ff8f5999f914a9c8ab5e0d1c3b60744714
Loyio Hex <i@loyio.me>
2022-11-30 22:34:17 +0800

    add minimum changes to make alternating binary string (#286)

---
+++ N1758-Minimum-Changes-To-Make-Alternating-Binary-String/solution1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c != ('0' + i % 2)) {
+                cnt++;
+            }
+        }
+        return min(cnt, (int)s.size() - cnt);
+    }
+};
