0d36045f87acd28d69a6d7ec85ed3d6b5fbd15cb
Loyio Hex <i@loyio.me>
2022-12-06 21:22:56 +0800

    add delivering boxes from storage to ports (#291)

---
+++ N1687-Delivering-Boxes-from-Storage-to-Ports/solution1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int boxDelivering(vector<vector<int>>& boxes, int portsCount, int maxBoxes, int maxWeight) {
+        int n = boxes.size();
+        vector<int> p(n + 1), w(n + 1), neg(n + 1);
+        vector<long long> W(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            p[i] = boxes[i - 1][0];
+            w[i] = boxes[i - 1][1];
+            if (i > 1) {
+                neg[i] = neg[i - 1] + (p[i - 1] != p[i]);
+            }
+            W[i] = W[i - 1] + w[i];
+        }
+        deque<int> opt = {0};
+        vector<int> f(n + 1), g(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            while (i - opt.front() > maxBoxes || W[i] - W[opt.front()] > maxWeight) {
+                opt.pop_front();
+            }
+            f[i] = g[opt.front()] + neg[i] + 2;
+            if (i != n) {
+                g[i] = f[i] - neg[i + 1];
+                while (!opt.empty() && g[i] <= g[opt.back()]) {
+                    opt.pop_back();
+                }
+                opt.push_back(i);
+            }
+        }
+        return f[n];
+    }
+};
