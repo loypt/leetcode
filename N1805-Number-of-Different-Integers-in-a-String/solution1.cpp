7ba21be748c2f4a5ca33822a6462702902f499cc
Loyio Hex <i@loyio.me>
2022-12-07 21:53:53 +0800

    add number of different integers in a string (#292)

---
+++ N1805-Number-of-Different-Integers-in-a-String/solution1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDifferentIntegers(string word) {
+        unordered_set<string> s;
+        int n = word.size(), p1 = 0, p2;
+        while (true) {
+            while (p1 < n && !isdigit(word[p1])) {
+                p1++;
+            }
+            if (p1 == n) {
+                break;
+            }
+            p2 = p1;
+            while (p2 < n && isdigit(word[p2])) {
+                p2++;
+            }
+            while (p2 - p1 > 1 && word[p1] == '0') {
+                p1++;
+            }
+            s.insert(word.substr(p1, p2 - p1));
+            p1 = p2;
+        }
+        return s.size();
+    }
+};
