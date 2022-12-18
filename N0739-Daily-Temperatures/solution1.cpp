3de88574a5a2f072c01ad66d40328d91207c21f4
Loyio Hex <i@loyio.me>
2022-12-18 11:26:02 +0800

    add minimum adjacent swaps for k consecutive ones

---
+++ N0739-Daily-Temperatures/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n);
+        stack<int> s;
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                int previousIndex = s.top();
+                ans[previousIndex] = i - previousIndex;
+                s.pop();
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
