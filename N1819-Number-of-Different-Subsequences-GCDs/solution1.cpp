4d650f8dbfa1f93ea0eab2093b83c10706d66b52
Loyio Hex <i@loyio.me>
2023-01-15 20:34:56 +0800

    add lexicographically smallest equivalent string (#326)

---
+++ N1819-Number-of-Different-Subsequences-GCDs/solution1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        vector<bool> occured(maxVal + 1, false);
+        for (int num : nums) {
+            occured[num] = true;
+        }
+        int ans = 0;
+        for (int i = 1; i <= maxVal; i++) {
+            int subGcd = 0;
+            for (int j = i; j <= maxVal; j += i) {
+                if (occured[j]) {
+                    if (subGcd == 0) {
+                        subGcd = j;
+                    } else {
+                        subGcd = __gcd(subGcd, j);
+                    }
+                    if (subGcd == i) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
