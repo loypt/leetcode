71690a2d88764cbae203c5e30c356c9c366acb3c
Loyio Hex <i@loyio.me>
2023-02-05 21:28:28 +0800

    add find all anagrams in a String

---
+++ N1210-Minimum-Moves-to-Reach-Target-with-Rotations/solution1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minimumMoves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<array<int, 2>>> dist(n, vector<array<int, 2>>(n, {-1, -1}));
+        dist[0][0][0] = 0;
+        queue<tuple<int, int, int>> q;
+        q.emplace(0, 0, 0);
+
+        while (!q.empty()) {
+            auto [x, y, status] = q.front();
+            q.pop();
+            if (status == 0) {
+                if (y + 2 < n && dist[x][y + 1][0] == -1 && grid[x][y + 2] == 0) {
+                    dist[x][y + 1][0] = dist[x][y][0] + 1;
+                    q.emplace(x, y + 1, 0);
+                }
+                if (x + 1 < n && dist[x + 1][y][0] == -1 && grid[x + 1][y] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x + 1][y][0] = dist[x][y][0] + 1;
+                    q.emplace(x + 1, y, 0);
+                }
+                if (x + 1 < n && y + 1 < n && dist[x][y][1] == -1 && grid[x + 1][y] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y][1] = dist[x][y][0] + 1;
+                    q.emplace(x, y, 1);
+                }
+            }
+            else {
+                if (y + 1 < n && dist[x][y + 1][1] == -1 && grid[x][y + 1] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y + 1][1] = dist[x][y][1] + 1;
+                    q.emplace(x, y + 1, 1);
+                }
+                if (x + 2 < n && dist[x + 1][y][1] == -1 && grid[x + 2][y] == 0) {
+                    dist[x + 1][y][1] = dist[x][y][1] + 1;
+                    q.emplace(x + 1, y, 1);
+                }
+                if (x + 1 < n && y + 1 < n && dist[x][y][0] == -1 && grid[x][y + 1] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y][0] = dist[x][y][1] + 1;
+                    q.emplace(x, y, 0);
+                }
+            }
+        }
+
+        return dist[n - 1][n - 2][0];
+    }
+};
