7fb61aeeb45ae928c966e85e3e676b9ac1dc981e
Loyio Hex <i@loyio.me>
2023-02-09 22:10:43 +0800

    add remove sub folders from the filesystem (#342)

---
+++ N1233-Remove-Sub-Folders-from-the-Filesystem/solution1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+        vector<string> ans = {folder[0]};
+        for (int i = 1; i < folder.size(); ++i) {
+            if (int pre = ans.end()[-1].size(); !(pre < folder[i].size() && ans.end()[-1] == folder[i].substr(0, pre) && folder[i][pre] == '/')) {
+                ans.push_back(folder[i]);
+            }
+        }
+        return ans;
+    }
+};
