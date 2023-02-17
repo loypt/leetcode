7a1b943564d9277782b1ac785c7c3f798e0e59c1
Loyio Hex <i@loyio.me>
2023-02-17 21:44:57 +0800

    add minimum distance between bst node

---
+++ N0783-Minimum-Distance-Between-BST-Nodes/solution1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int& pre, int& ans) {
+        if (root == nullptr) {
+            return;
+        }
+        dfs(root->left, pre, ans);
+        if (pre == -1) {
+            pre = root->val;
+        } else {
+            ans = min(ans, root->val - pre);
+            pre = root->val;
+        }
+        dfs(root->right, pre, ans);
+    }
+    int minDiffInBST(TreeNode* root) {
+        int ans = INT_MAX, pre = -1;
+        dfs(root, pre, ans);
+        return ans;
+    }
+};
