ee18df3cb4941e0d270247028dfc386b83a74fb9
Loyio Hex <i@loyio.me>
2023-02-18 21:31:48 +0800

    add minimum distance between bst node (#351)

---
+++ N1139-Largest-1-Bordered-Square/solution1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> left(m + 1, vector<int>(n + 1));
+        vector<vector<int>> up(m + 1, vector<int>(n + 1));
+        int maxBorder = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (grid[i - 1][j - 1] == 1) {
+                    left[i][j] = left[i][j - 1] + 1;
+                    up[i][j] = up[i - 1][j] + 1;
+                    int border = min(left[i][j], up[i][j]);
+                    while (left[i - border + 1][j] < border || up[i][j - border + 1] < border) {
+                        border--;
+                    }
+                    maxBorder = max(maxBorder, border);
+                }
+            }
+        }
+        return maxBorder * maxBorder;
+    }
+};
