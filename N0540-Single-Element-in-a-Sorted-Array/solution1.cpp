dd03021b90f866fff1d82d5bbeb6a17b6971bf57
Loyio Hex <i@loyio.me>
2023-02-21 21:31:15 +0800

    add single element in a sorted array

---
+++ N0540-Single-Element-in-a-Sorted-Array/solution1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
