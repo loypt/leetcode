f1027274bce7a25073b46a69cf2d4bd7732c72d9
Loyio Hex <i@loyio.me>
2023-02-22 21:18:36 +0800

    add single element in a sorted array (#355)

---
+++ N1326-Minimum-Number-of-Taps-to-Open-to-Water-a-Garden/solution1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<pair<int, int>> intervals;
+        for (int i = 0; i <= n; i++) {
+            int start = max(0, i - ranges[i]);
+            int end = min(n, i + ranges[i]);
+            intervals.emplace_back(start, end);
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (auto [start, end] : intervals) {
+            if (dp[start] == INT_MAX) {
+                return -1;
+            }
+            for (int j = start; j <= end; j++) {
+                dp[j] = min(dp[j], dp[start] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
