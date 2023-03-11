81d052fb2a8ccb4d34f3d0ab3fbe45efb8f28ff0
Loyio Hex <i@loyio.me>
2023-03-11 22:10:23 +0800

    add linked list random node (#373)

---
+++ N1590-Make-Sum-Divisible-by-P/solution1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int x = 0;
+        for (auto num : nums) {
+            x = (x + num) % p;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        unordered_map<int, int> index;
+        int y = 0, res = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            index[y] = i;
+            y = (y + nums[i]) % p;
+            if (index.count((y - x + p) % p) > 0) {
+                res = min(res, i - index[(y - x + p) % p] + 1);
+            }
+        }
+        return res == nums.size() ? -1 : res;
+    }
+};
