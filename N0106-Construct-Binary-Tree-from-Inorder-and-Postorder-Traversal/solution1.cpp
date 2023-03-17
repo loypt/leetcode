3d39397a17330119749c108f54a0d1758bcbc369
Loyio Hex <i@loyio.me>
2023-03-17 22:32:45 +0800

    add count subarrays with median k (#379)

---
+++ N0106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution1.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int post_idx;
+    unordered_map<int, int> idx_map;
+public:
+    TreeNode* helper(int in_left, int in_right, vector<int>& inorder, vector<int>& postorder){
+        if (in_left > in_right) {
+            return nullptr;
+        }
+
+        int root_val = postorder[post_idx];
+        TreeNode* root = new TreeNode(root_val);
+
+        int index = idx_map[root_val];
+
+        post_idx--;
+        root->right = helper(index + 1, in_right, inorder, postorder);
+        root->left = helper(in_left, index - 1, inorder, postorder);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        post_idx = (int)postorder.size() - 1;
+
+        int idx = 0;
+        for (auto& val : inorder) {
+            idx_map[val] = idx++;
+        }
+        return helper(0, (int)inorder.size() - 1, inorder, postorder);
+    }
+};
