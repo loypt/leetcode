5c4471e3d49baf09d83f6216c7a956bcfc8ca8e6
Loyio Hex <i@loyio.me>
2023-03-25 22:43:58 +0800

    add stream of characters (#386)

---
+++ N1032-Stream-of-Characters/solution1.cpp
@@ -0,0 +1,66 @@
+typedef struct TrieNode {
+    vector<TrieNode *> children;
+    bool isEnd;
+    TrieNode *fail;
+    TrieNode() {
+        this->children = vector<TrieNode *>(26, nullptr);
+        this->isEnd = false;
+        this->fail = nullptr;
+    }
+};
+
+class StreamChecker {
+public:
+    TrieNode *root;
+    TrieNode *temp;
+    StreamChecker(vector<string>& words) {
+        root = new TrieNode();
+        for (string &word : words) {
+            TrieNode *cur = root;
+            for (int i = 0; i < word.size(); i++) {
+                int index = word[i] - 'a';
+                if (cur->children[index] == nullptr) {
+                    cur->children[index] = new TrieNode();
+                }
+                cur = cur->children[index];
+            }
+            cur->isEnd = true;
+        }
+        root->fail = root;
+        queue<TrieNode *> q;
+        for (int i = 0; i < 26; i++) {
+            if(root->children[i] != nullptr) {
+                root->children[i]->fail = root;
+                q.emplace(root->children[i]);
+            } else {
+                root->children[i] = root;
+            }
+        }
+        while (!q.empty()) {
+            TrieNode *node = q.front();
+            q.pop();
+            node->isEnd = node->isEnd || node->fail->isEnd;
+            for (int i = 0; i < 26; i++) {
+                if(node->children[i] != nullptr) {
+                    node->children[i]->fail = node->fail->children[i];
+                    q.emplace(node->children[i]);
+                } else {
+                    node->children[i] = node->fail->children[i];
+                }
+            }
+        }
+
+        temp = root;
+    }
+    
+    bool query(char letter) {
+        temp = temp->children[letter - 'a'];
+        return temp->isEnd;
+    }
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
