c13d5a071a40c128c26d92395813cb3c31488949
Loyio Hex <i@loyio.me>
2023-04-07 23:42:39 +0800

    add moving stones until consectutive

---
+++ N1020-Number-of-Enclaves/solution1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        this->m = grid.size();
+        this->n = grid[0].size();
+        this->visited = vector<vector<bool>>(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+        for (int j = 1; j < n - 1; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m - 1, j);
+        }
+        int enclaves = 0;
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    enclaves++;
+                }
+            }
+        }
+        return enclaves;
+    }
+
+    void dfs(const vector<vector<int>> & grid, int row, int col) {
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0 || visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        for (auto & dir : dirs) {
+            dfs(grid, row + dir[0], col + dir[1]);
+        }
+    }
+private:
+    int m, n;
+    vector<vector<bool>> visited;
+};
