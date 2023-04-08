a3edd55c9aaa3ff00347f0013420d4f0dc8944f8
Loyio Hex <i@loyio.me>
2023-04-08 22:34:47 +0800

    add moving stones until consectutive (#400)

---
+++ N1040-Moving-Stones-Until-Consecutive-II/solution1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) {
+        int n = stones.size();
+        sort(stones.begin(), stones.end());
+        if (stones.back() - stones[0] + 1 == n) {
+            return {0, 0};
+        }
+        int ma = max(stones[n - 2] - stones[0] + 1, stones[n - 1] - stones[1] + 1) - (n - 1);
+        int mi = n;
+        for (int i = 0, j = 0; i < n && j + 1 < n; ++i) {
+            while (j + 1 < n && stones[j + 1] - stones[i] + 1 <= n) {
+                ++j;
+            }
+            if (j - i + 1 == n - 1 && stones[j] - stones[i] + 1 == n - 1) {
+                mi = min(mi, 2);
+            } else {
+                mi = min(mi, n - (j - i + 1));
+            }
+        }
+        return {mi, ma};
+    }
+};
