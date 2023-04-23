6651e126f646189a40176cd6290316616965d1c7
Loyio Hex <i@loyio.me>
2023-04-23 23:13:44 +0800

    add restore the array

---
+++ N1105-Filling-Bookcase-Shelves/solution1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+        vector<int> dp(n + 1, 1000000);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            int maxHeight = 0, curWidth = 0;
+            for (int j = i; j >= 0; --j) {
+                curWidth += books[j][0];
+                if (curWidth > shelfWidth) {
+                    break;
+                }
+                maxHeight = max(maxHeight, books[j][1]);
+                dp[i + 1] = min(dp[i + 1], dp[j] + maxHeight);
+            }
+        }
+        return dp[n];
+    }
+};
