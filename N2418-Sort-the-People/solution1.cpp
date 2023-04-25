bb8cdbdb548b14466073ab403139a493856f09f1
Loyio Hex <i@loyio.me>
2023-04-25 22:49:25 +0800

    add smallest number in infinite set

---
+++ N2418-Sort-the-People/solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = names.size();
+        vector<int> indices(n);
+        iota(indices.begin(), indices.end(), 0);
+        sort(indices.begin(), indices.end(), [&](int x, int y) {
+            return heights[x] > heights[y];
+        });
+        vector<string> res(n);
+        for (int i = 0; i < n; i++) {
+            res[i] = names[indices[i]];
+        }
+        return res;
+    }
+};
