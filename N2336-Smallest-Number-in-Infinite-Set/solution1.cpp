6eaf95a007a8743146c37276a231451375e59cb4
Loyio Hex <i@loyio.me>
2023-04-26 22:37:45 +0800

    add smallest number in infinite set (#417)

---
+++ N2336-Smallest-Number-in-Infinite-Set/solution1.cpp
@@ -0,0 +1,27 @@
+class SmallestInfiniteSet {
+public:
+    set<int>st;
+    SmallestInfiniteSet() {
+        st.clear();
+        for(int i=1;i<=1000;i++){
+            st.insert(i);
+        }
+    }
+    
+    int popSmallest() {
+        int t = *st.begin();
+        st.erase(st.begin());
+        return t;
+    }
+    
+    void addBack(int num) {
+        st.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
