f2c1541007f95ebc00f4c1e1a25e93af25c509a5
Loyio Hex <i@loyio.me>
2023-05-05 21:56:34 +0800

    add dota2 senate (#426)

---
+++ N2106-Maximum-Fruits-Harvested-After-at-Most-K-Steps/solution1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        vector<int> sum(n + 1);
+        vector<int> indices(n);
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + fruits[i][1];
+            indices[i] = fruits[i][0];
+        }
+        int ans = 0;
+        for (int x = 0; x <= k / 2; x++) {
+            int y = k - 2 * x;
+            int left = startPos - x;
+            int right = startPos + y;
+            int start = lower_bound(indices.begin(), indices.end(), left) - indices.begin();
+            int end = upper_bound(indices.begin(), indices.end(), right) - indices.begin();
+            ans = max(ans, sum[end] - sum[start]);
+            y = k - 2 * x;
+            left = startPos - y;
+            right = startPos + x;
+            start = lower_bound(indices.begin(), indices.end(), left) - indices.begin();
+            end = upper_bound(indices.begin(), indices.end(), right) - indices.begin();
+            ans = max(ans, sum[end] - sum[start]);
+        }
+        return ans;
+    }
+};
