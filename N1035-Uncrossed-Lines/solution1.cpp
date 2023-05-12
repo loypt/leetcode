ada4e7c1562b0bcc1183ec1d569d3f82e2f3f58b
Loyio Hex <i@loyio.me>
2023-05-12 22:12:28 +0800

    add binary string with substrings representing 1 to N (#433)

---
+++ N1035-Uncrossed-Lines/solution1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; i++) {
+            int num1 = nums1[i - 1];
+            for (int j = 1; j <= n; j++) {
+                int num2 = nums2[j - 1];
+                if (num1 == num2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
