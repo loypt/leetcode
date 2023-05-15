fccb86e71dbf00553a2c76aea8cce0c5a2c8d970
Loyio Hex <i@loyio.me>
2023-05-15 23:00:34 +0800

    add distant barcodes (#436)

---
+++ N1054-Distant-Barcodes/solution1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        unordered_map<int, int> count;
+        for (int b : barcodes) {
+            count[b]++;
+        }
+        priority_queue<pair<int, int>> q;
+        for (const auto &[x, cx] : count) {
+            q.push({cx, x});
+        }
+        vector<int> res;
+        while (q.size()) {
+            auto [cx, x] = q.top();
+            q.pop();
+            if (res.empty() || res.back() != x) {
+                res.push_back(x);
+                if (cx > 1) {
+                    q.push({cx - 1, x});
+                }
+            } else {
+                if (q.size() < 1) return res;
+                auto [cy, y] = q.top();
+                q.pop();
+                res.push_back(y);
+                if (cy > 1)  {
+                    q.push({cy - 1, y});
+                }
+                q.push({cx, x});
+            }
+        }
+        return res;
+    }
+};
