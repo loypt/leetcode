acfdcbca36b9f0725da09cfb9ec0269e2fe9a2d1
Loyio Hex <i@loyio.me>
2023-05-25 23:14:08 +0800

    add new 21 game

---
+++ N0837-New-21-Game/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0) {
+            return 1.0;
+        }
+        vector<double> dp(k + maxPts);
+        for (int i = k; i <= n && i < k + maxPts; i++) {
+            dp[i] = 1.0;
+        }
+        dp[k - 1] = 1.0 * min(n - k + 1, maxPts) / maxPts;
+        for (int i = k - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] - (dp[i + maxPts + 1] - dp[i + 1]) / maxPts;
+        }
+        return dp[0];
+    }
+};
