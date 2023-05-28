5cd602fa6abaadee6d006ab4d09c20d1b1d026dd
Loyio Hex <i@loyio.me>
2023-05-28 21:12:34 +0800

    add minimum cost to cut a stick

---
+++ N1547-Minimum-Cost-to-Cut-a-Stick/solution1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int m = cuts.size();
+        sort(cuts.begin(), cuts.end());
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        vector<vector<int>> f(m + 2, vector<int>(m + 2));
+        for (int i = m; i >= 1; --i) {
+            for (int j = i; j <= m; ++j) {
+                f[i][j] = (i == j ? 0 : INT_MAX);
+                for (int k = i; k <= j; ++k) {
+                    f[i][j] = min(f[i][j], f[i][k - 1] + f[k + 1][j]);
+                }
+                f[i][j] += cuts[j + 1] - cuts[i - 1];
+            }
+        }
+        return f[1][m];
+    }
+};
