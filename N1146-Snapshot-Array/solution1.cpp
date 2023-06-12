1c213b5e278c0207aea510924cd6918d35c87a14
Loyio Hex <i@loyio.me>
2023-06-12 21:35:59 +0800

    add snapshot array (#462)

---
+++ N1146-Snapshot-Array/solution1.cpp
@@ -0,0 +1,29 @@
+class SnapshotArray {
+public:
+    unordered_map<int, map<int, int>> a;
+    int cur_snap = 0;
+    SnapshotArray(int length) {
+
+    }
+    
+    void set(int index, int val) {
+        a[index][cur_snap] = val; 
+    }
+    
+    int snap() {
+        return cur_snap++;
+    }
+    
+    int get(int index, int snap_id) {
+        auto it = a[index].upper_bound(snap_id);
+        return it == begin(a[index]) ? 0 : prev(it)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
