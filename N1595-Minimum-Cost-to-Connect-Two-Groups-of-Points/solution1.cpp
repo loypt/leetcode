f4e54f3206ba2b9deebd130cf0b38942470075ef
Loyio Hex <i@loyio.me>
2023-06-20 22:27:03 +0800

    add k radius subarray averages

---
+++ N1595-Minimum-Cost-to-Connect-Two-Groups-of-Points/solution1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int connectTwoGroups(vector<vector<int>>& cost) {
+        int size1 = cost.size(), size2 = cost[0].size(), m = 1 << size2;
+        vector<int> dp1(m, INT_MAX / 2), dp2(m);
+        dp1[0] = 0;
+        for (int i = 1; i <= size1; i++) {
+            for (int s = 0; s < m; s++) {
+                dp2[s] = INT_MAX / 2;
+                for (int k = 0; k < size2; k++) {
+                    if ((s & (1 << k)) == 0) {
+                        continue;
+                    }
+                    dp2[s] = min(dp2[s], dp2[s ^ (1 << k)] + cost[i - 1][k]);
+                    dp2[s] = min(dp2[s], dp1[s] + cost[i - 1][k]);
+                    dp2[s] = min(dp2[s], dp1[s ^ (1 << k)] + cost[i - 1][k]);
+                }
+            }
+            dp1.swap(dp2);
+        }
+        return dp1[m - 1];
+    }
+};
