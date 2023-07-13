23632bf94f84a6fcbca8492b213cdd7c6c09d6ab
Loyio Hex <i@loyio.me>
2023-07-13 22:36:25 +0800

    add find eventual safe states (#491)

---
+++ N0802-Find-Eventual-Safe-States/solution1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph) {
+        int n = graph.size();
+        vector<int> color(n);
+
+        function<bool(int)> safe = [&](int x) {
+            if (color[x] > 0) {
+                return color[x] == 2;
+            }
+            color[x] = 1;
+            for (int y : graph[x]) {
+                if (!safe(y)) {
+                    return false;
+                }
+            }
+            color[x] = 2;
+            return true;
+        };
+
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            if (safe(i)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
