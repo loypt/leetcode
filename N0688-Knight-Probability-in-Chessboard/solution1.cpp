d9deaf45aca435b1ce6c57bae403544b4db54e58
Loyio Hex <i@loyio.me>
2023-07-23 22:22:33 +0800

    add knight probability in chessboard (#498)

---
+++ N0688-Knight-Probability-in-Chessboard/solution1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>>(n, vector<double>(n)));
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        dp[step][i][j] = 1;
+                    } else {
+                        for (auto & dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[k][row][column];
+    }
+};
