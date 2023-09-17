d063dd25e7be54edb6d61f96682d827fd32d3bf6
Loyio Hex <i@loyio.me>
2023-09-17 21:44:08 +0800

    add shortest path visiting all nodes

---
+++ N0847-Shortest-Path-Visiting-All-Nodes/solution1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        queue<tuple<int, int, int>> q;
+        vector<vector<int>> seen(n, vector<int>(1 << n));
+        for (int i = 0; i < n; ++i) {
+            q.emplace(i, 1 << i, 0);
+            seen[i][1 << i] = true;
+        }
+
+        int ans = 0;
+        while (!q.empty()) {
+            auto [u, mask, dist] = q.front();
+            q.pop();
+            if (mask == (1 << n) - 1) {
+                ans = dist;
+                break;
+            }
+            for (int v: graph[u]) {
+                int mask_v = mask | (1 << v);
+                if (!seen[v][mask_v]) {
+                    q.emplace(v, mask_v, dist + 1);
+                    seen[v][mask_v] = true;
+                }
+            }
+        }
+        return ans;
+    }
+};
