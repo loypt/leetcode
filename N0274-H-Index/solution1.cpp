2e82d2be155b003177a102a906606598e51cf106
Loyio Hex <i@loyio.me>
2023-10-30 21:58:15 +0800

    add h index (#568)

---
+++ N0274-H-Index/solution1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int h = 0, i = citations.size() - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
+        }
+        return h;
+    }
+};
