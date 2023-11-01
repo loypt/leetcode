49dcf359682b11fde54771fe2e4513feec79c297
Loyio Hex <i@loyio.me>
2023-11-01 22:49:00 +0800

    add maximum employees to be invited to a meeting

---
+++ N0501-Find-Mode-in-Binary-Search-Tree/solution1.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int base, count, maxCount;
+    vector<int> answer;
+
+    void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.push_back(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer = vector<int> {base};
+        }
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        TreeNode *cur = root, *pre = nullptr;
+        while (cur) {
+            if (!cur->left) {
+                update(cur->val);
+                cur = cur->right;
+                continue;
+            }
+            pre = cur->left;
+            while (pre->right && pre->right != cur) {
+                pre = pre->right;
+            }
+            if (!pre->right) {
+                pre->right = cur;
+                cur = cur->left;
+            } else {
+                pre->right = nullptr;
+                update(cur->val);
+                cur = cur->right;
+            }
+        }
+        return answer;
+    }
+};
