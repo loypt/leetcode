d65629689cfa75ed812f1fbaafd50b182ff92089
Loyio Hex <i@loyio.me>
2023-11-02 21:43:30 +0800

    add maximum employees to be invited to a meeting (#572)

---
+++ N2127-Maximum-Employees-to-Be-Invited-to-a-Meeting/solution1.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int maximumInvitations(vector<int>& favorite) {
+        int n = favorite.size();
+        vector<int> indeg(n);
+        for (int i = 0; i < n; ++i) {
+            ++indeg[favorite[i]];
+        }
+        vector<int> used(n), f(n, 1);
+        queue<int> q;
+        for (int i = 0; i < n; ++i) {
+            if (!indeg[i]) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int u = q.front();
+            used[u] = true;
+            q.pop();
+            int v = favorite[u];
+            f[v] = max(f[v], f[u] + 1);
+            --indeg[v];
+            if (!indeg[v]) {
+                q.push(v);
+            }
+        }
+        int ring = 0, total = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!used[i]) {
+                int j = favorite[i];
+                if (favorite[j] == i) {
+                    total += f[i] + f[j];
+                    used[i] = used[j] = true;
+                }
+                else {
+                    int u = i, cnt = 0;
+                    while (true) {
+                        ++cnt;
+                        u = favorite[u];
+                        used[u] = true;
+                        if (u == i) {
+                            break;
+                        }
+                    }
+                    ring = max(ring, cnt);
+                }
+            }
+        }
+        return max(ring, total);
+    }
+};
