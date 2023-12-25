3fa939bfd546e9b7dfcee4a0c7faa00af76b7e3e
Loyio Hex <i@loyio.me>
2023-12-25 21:27:11 +0800

    add minimum garden perimeter to collect enough apples (#622)

---
+++ N1954-Minimum-Garden-Perimeter-to-Collect-Enough-Apples/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumPerimeter(long long neededApples) {
+        long long left = 1, right = 100000, ans = 0;
+        while (left <= right) {
+            long long mid = (left + right) / 2;
+            if (2 * mid * (mid + 1) * (mid * 2 + 1) >= neededApples) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans * 8;
+    }
+};
