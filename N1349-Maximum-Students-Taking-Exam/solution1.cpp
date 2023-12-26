055ef103edca1ded630c7a961683f179a113ca10
Loyio Hex <i@loyio.me>
2023-12-26 21:53:37 +0800

    add maximum students taking exam

---
+++ N1349-Maximum-Students-Taking-Exam/solution1.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int maxStudents(vector<vector<char>>& seats) {
+        int m = seats.size(), n = seats[0].size();
+        unordered_map<int, int> memo;
+
+        auto isSingleRowCompliant = [&](int status, int row) -> bool {
+            for (int j = 0; j < n; j++) {
+                if ((status >> j) & 1) {
+                    if (seats[row][j] == '#') {
+                        return false;
+                    }
+                    if (j > 0 && ((status >> (j - 1)) & 1)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        };
+        
+        auto isCrossRowsCompliant = [&](int status, int upperRowStatus) -> bool {
+            for (int j = 0; j < n; j++) {
+                if ((status >> j) & 1) {
+                    if (j > 0 && ((upperRowStatus >> (j - 1)) & 1)) {
+                        return false;
+                    }
+                    if (j < n - 1 && ((upperRowStatus >> (j + 1)) & 1)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        };
+
+        function<int(int, int)> dp = [&](int row, int status) -> int {
+            int key = (row << n) + status;
+            if (!memo.count(key)) {
+                if (!isSingleRowCompliant(status, row)) {
+                    memo[key] = INT_MIN;
+                    return INT_MIN;
+                }
+                int students = __builtin_popcount(status);
+                if (row == 0) {
+                    memo[key] = students;
+                    return students;
+                }
+                int mx = 0;
+                for (int upperRowStatus = 0; upperRowStatus < 1 << n; upperRowStatus++) {
+                    if (isCrossRowsCompliant(status, upperRowStatus)) {
+                        mx = max(mx, dp(row - 1, upperRowStatus));
+                    }
+                }
+                memo[key] = students + mx;
+            }
+            return memo[key];
+        };
+        
+        int mx = 0;
+        for (int i = 0; i < (1 << n); i++) {
+            mx = max(mx, dp(m - 1, i));
+        }
+        return mx;
+    }
+};
