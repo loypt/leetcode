0c8be1d43ee5fd4b420e8b4f2b955b64504471e3
Loyio Hex <i@loyio.me>
2024-01-09 22:06:08 +0800

    add number of boomerangs (#635)

---
+++ N0447-Number-of-Boomerangs/solution1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>> &points) {
+        int ans = 0;
+        for (auto &p : points) {
+            unordered_map<int, int> cnt;
+            for (auto &q : points) {
+                int dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                ++cnt[dis];
+            }
+            for (auto &[_, m] : cnt) {
+                ans += m * (m - 1);
+            }
+        }
+        return ans;
+    }
+};
