3613b01907f7d1acb1021d98e3188ed6138e77da
Loyio Hex <i@loyio.me>
2024-02-27 23:08:27 +0800

    add count valid paths in a tree

---
+++ N0543-Diameter-of-Binary-Tree/solution1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int ans;
+    int depth(TreeNode* rt){
+        if (rt == NULL) {
+            return 0;
+        }
+        int L = depth(rt->left);
+        int R = depth(rt->right);
+        ans = max(ans, L + R + 1);
+        return max(L, R) + 1;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+};
