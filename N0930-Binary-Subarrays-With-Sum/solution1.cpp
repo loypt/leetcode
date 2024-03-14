ab294808f14bfccc2dd6beffedfc240fdcd71c3c
Loyio Hex <i@loyio.me>
2024-03-14 22:14:31 +0800

    add largest element in an array after merge operations

---
+++ N0930-Binary-Subarrays-With-Sum/solution1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        int left1 = 0, left2 = 0, right = 0;
+        int sum1 = 0, sum2 = 0;
+        int ret = 0;
+        while (right < n) {
+            sum1 += nums[right];
+            while (left1 <= right && sum1 > goal) {
+                sum1 -= nums[left1];
+                left1++;
+            }
+            sum2 += nums[right];
+            while (left2 <= right && sum2 >= goal) {
+                sum2 -= nums[left2];
+                left2++;
+            }
+            ret += left2 - left1;
+            right++;
+        }
+        return ret;
+    }
+};
