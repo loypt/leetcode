6cd511420ce0cf55a6208c63b7853b4e234ff937
Loyio Hex <i@loyio.me>
2024-03-31 23:13:22 +0800

    add minimum number of coins to be added (#690)

---
+++ N0992-Subarrays-with-K-Different-Integers/solution1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMostK(nums, k) - atMostK(nums, k - 1);
+    }
+    int atMostK(vector<int>& A, int K) {
+        int i = 0, res = 0;
+        unordered_map<int, int> count;
+        for (int j = 0; j < A.size(); ++j) {
+            if (!count[A[j]]++) K--;
+            while (K < 0) {
+                if (!--count[A[i]]) K++;
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+};
