c39836bdd13f3f4108b799d0670299029176f36d
Loyio Hex <i@loyio.me>
2024-04-18 22:53:06 +0800

    add island perimeter

---
+++ N0463-Island-Perimeter/solution1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    constexpr static int dx[4] = {0, 1, 0, -1};
+    constexpr static int dy[4] = {1, 0, -1, 0};
+public:
+    int islandPerimeter(vector<vector<int>> &grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j]) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || !grid[tx][ty]) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
+    }
+};
