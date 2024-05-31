4646df233de45ca3e5df5dfdf76ae43fb9ae3df1
Loyio Hex <i@loyio.me>
2024-05-31 22:47:20 +0800

    add count triplets that can form two arraysof equal xor (#730)

---
+++ N1442-Count-Triplets-That-Can-Form-Two-Arrays-of-Equal-XOR/solution1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countTriplets(vector<int> &arr) {
+        int n = arr.size();
+        unordered_map<int, int> cnt, total;
+        int ans = 0, s = 0;
+        for (int k = 0; k < n; ++k) {
+            int val = arr[k];
+            if (cnt.count(s ^ val)) {
+                ans += cnt[s ^ val] * k - total[s ^ val];
+            }
+            ++cnt[s];
+            total[s] += k;
+            s ^= val;
+        }
+        return ans;
+    }
+};
