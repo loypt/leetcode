848bbb616936c4059f6ed01f0934545e2f0ea1d3
Loyio Hex <i@loyio.me>
2024-06-01 22:13:24 +0800

    add score of a string

---
+++ N2928-Distribute-Candies-Among-Children-I/solution1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int cal(int x) {
+        if (x < 0) {
+            return 0;
+        }
+        return x * (x - 1) / 2;
+    }
+
+    int distributeCandies(int n, int limit) {
+        return cal(n + 2) - 3 * cal(n - limit + 1) + 3 * cal(n - (limit + 1) * 2 + 2) - cal(n - 3 * (limit + 1) + 2);
+    }
+};
