1b823e7352c0922791d5dec6c260c1ab240dee74
Loyio Hex <i@loyio.me>
2024-06-05 22:41:08 +0800

    add find common characters

---
+++ N1002-Find-Common-Characters/solution1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minfreq(26, INT_MAX);
+        vector<int> freq(26);
+        for (const string& word: words) {
+            fill(freq.begin(), freq.end(), 0);
+            for (char ch: word) {
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minfreq[i] = min(minfreq[i], freq[i]);
+            }
+        }
+
+        vector<string> ans;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minfreq[i]; ++j) {
+                ans.emplace_back(1, i + 'a');
+            }
+        }
+        return ans;
+    }
+};
