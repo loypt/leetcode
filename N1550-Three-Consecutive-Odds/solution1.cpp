21f4e882f28b7671adeed797b7bf73bf719da0c2
Loyio Hex <i@loyio.me>
2024-07-02 22:54:58 +0800

    add maximum path quality of a graph (#760)

---
+++ N1550-Three-Consecutive-Odds/solution1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i <= n - 3; ++i) {
+            if ((arr[i] & 1) & (arr[i + 1] & 1) & (arr[i + 2] & 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
