d569b350f70e9bfb59d5f245663c7e1df9501a1d
Loyio Hex <i@loyio.me>
2024-07-26 23:57:22 +0800

    find the value of the partition

---
+++ N2740-Find-the-Value-of-the-Partition/solution1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = INT_MAX;
+        for (int i = 1; i < nums.size(); i++) {
+            res = min(res, nums[i] - nums[i - 1]);
+        }
+        return res;
+    }
+};
