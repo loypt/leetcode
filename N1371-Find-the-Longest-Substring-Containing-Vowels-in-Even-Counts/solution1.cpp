9aad95c5970e28e2938e24363411da3cdde38674
Loyio Hex <i@loyio.me>
2024-09-15 22:26:48 +0800

    add points that intersect with cars

---
+++ N1371-Find-the-Longest-Substring-Containing-Vowels-in-Even-Counts/solution1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        int ans = 0, status = 0, n = s.length();
+        vector<int> pos(1 << 5, -1);
+        pos[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == 'a') {
+                status ^= 1<<0;
+            } else if (s[i] == 'e') {
+                status ^= 1<<1;
+            } else if (s[i] == 'i') {
+                status ^= 1<<2;
+            } else if (s[i] == 'o') {
+                status ^= 1<<3;
+            } else if (s[i] == 'u') {
+                status ^= 1<<4;
+            }
+            if (~pos[status]) {
+                ans = max(ans, i + 1 - pos[status]);
+            } else {
+                pos[status] = i + 1;
+            }
+        }
+        return ans;
+    }
+};
