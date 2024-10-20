7041d1d42cc61178a3e6cd8124aedea9a1f638e0
Loyio Hex <i@loyio.me>
2024-10-20 21:05:47 +0800

    add find kth bit in nth binary string (#854)

---
+++ N1545-Find-Kth-Bit-in-Nth-Binary-String/solution1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if (k == 1) {
+            return '0';
+        }
+        int mid = 1 << (n - 1);
+        if (k == mid) {
+            return '1';
+        } else if (k < mid) {
+            return findKthBit(n - 1, k);
+        } else {
+            k = mid * 2 - k;
+            return invert(findKthBit(n - 1, k));
+        }
+    }
+
+    char invert(char bit) {
+        return (char) ('0' + '1' - bit);
+    }
+};
