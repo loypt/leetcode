c49c923933b8cae070ea2e219ba1576509add3e3
Loyio Hex <i@loyio.me>
2024-11-04 21:47:15 +0800

    add shopping offers (#868)

---
+++ N0638-Shopping-Offers/solution1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    map<vector<int>, int> memo;
+
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        int n = price.size();
+
+        vector<vector<int>> filterSpecial;
+        for (auto & sp : special) {
+            int totalCount = 0, totalPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                totalCount += sp[i];
+                totalPrice += sp[i] * price[i];
+            }
+            if (totalCount > 0 && totalPrice > sp[n]) {
+                filterSpecial.emplace_back(sp);
+            }
+        }
+
+        return dfs(price, special, needs, filterSpecial, n);
+    }
+
+    int dfs(vector<int> price,const vector<vector<int>> & special, vector<int> curNeeds, vector<vector<int>> & filterSpecial, int n) {
+        if (!memo.count(curNeeds)) {
+            int minPrice = 0;
+            for (int i = 0; i < n; ++i) {
+                minPrice += curNeeds[i] * price[i];
+            }
+            for (auto & curSpecial : filterSpecial) {
+                int specialPrice = curSpecial[n];
+                vector<int> nxtNeeds;
+                for (int i = 0; i < n; ++i) {
+                    if (curSpecial[i] > curNeeds[i]) {
+                        break;
+                    }
+                    nxtNeeds.emplace_back(curNeeds[i] - curSpecial[i]);
+                }
+                if (nxtNeeds.size() == n) {
+                    minPrice = min(minPrice, dfs(price, special, nxtNeeds, filterSpecial, n) + specialPrice);
+                }
+            }
+            memo[curNeeds] = minPrice;
+        }
+        return memo[curNeeds];
+    }
+};
