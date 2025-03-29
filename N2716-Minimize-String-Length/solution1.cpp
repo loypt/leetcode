89a59ebcfcc13d33ecb81653a2428e74b514c7ec
Loyio Hex <i@loyio.me>
2025-03-29 23:17:07 +0800

    add maximum number of points from grid queries (#977)

---
+++ N2716-Minimize-String-Length/solution1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        int mask = 0;
+        for (char c : s) {
+            mask |= 1 << (c - 'a');
+        }
+        return __builtin_popcount(mask);
+    }
+};
