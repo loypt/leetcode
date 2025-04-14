ffcdfaf3a3073b3ea4ad7c3f57a263fbb6eebff1
Loyio Hex <i@loyio.me>
2025-04-14 22:04:09 +0800

    add count good tripletes

---
+++ N1534-Count-Good-Triplets/solution1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int ans = 0, n = arr.size();
+        vector<int> sum(1001, 0);
+        for (int j = 0; j < n; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                if (abs(arr[j] - arr[k]) <= b) {
+                    int lj = arr[j] - a, rj = arr[j] + a;
+                    int lk = arr[k] - c, rk = arr[k] + c;
+                    int l = max(0, max(lj, lk)), r = min(1000, min(rj, rk));
+                    if (l <= r) {
+                        if (l == 0) {
+                            ans += sum[r];
+                        }
+                        else {
+                            ans += sum[r] - sum[l - 1];
+                        }
+                    }
+                }
+            }
+            for (int k = arr[j]; k <= 1000; ++k) {
+                ++sum[k];
+            }
+        }
+        return ans;
+    }
+};
