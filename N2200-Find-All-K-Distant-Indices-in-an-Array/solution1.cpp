dd124e96253a1dcf5afadfc8f521a97b607708a8
Loyio Hex <i@loyio.me>
2025-06-24 21:36:26 +0800

    add find all k distant indices in an array

---
+++ N2200-Find-All-K-Distant-Indices-in-an-Array/solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> res;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (nums[j] == key && abs(i - j) <= k) {
+                    res.push_back(i);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
