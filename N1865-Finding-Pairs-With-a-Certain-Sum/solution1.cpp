0bdda387dbb19e5c2648ef82573b39d778253628
Loyio Hex <i@loyio.me>
2025-07-06 14:12:36 +0800

    add finding pairs with a certain sum

---
+++ N1865-Finding-Pairs-With-a-Certain-Sum/solution1.cpp
@@ -0,0 +1,38 @@
+class FindSumPairs {
+private:
+    vector<int> nums1, nums2;
+    unordered_map<int, int> cnt;
+
+public:
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        this->nums1 = nums1;
+        this->nums2 = nums2;
+        for (int num: nums2) {
+            ++cnt[num];
+        }
+    }
+    
+    void add(int index, int val) {
+        --cnt[nums2[index]];
+        nums2[index] += val;
+        ++cnt[nums2[index]];
+    }
+    
+    int count(int tot) {
+        int ans = 0;
+        for (int num: nums1) {
+            int rest = tot - num;
+            if (cnt.count(rest)) {
+                ans += cnt[rest];
+            }
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
