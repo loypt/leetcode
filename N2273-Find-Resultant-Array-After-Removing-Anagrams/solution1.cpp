19967df0007d02cd3f3735b7d3d1a936cd8e1ed8
Loyio Hex <i@loyio.me>
2025-10-13 22:39:56 +0800

    add find resultant array after removing anagrams

---
+++ N2273-Find-Resultant-Array-After-Removing-Anagrams/solution1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> res = {words[0]};
+        int n = words.size();
+        auto compare = [](const string& word1, const string& word2) -> bool {
+            vector<int> freq(26);
+            for (char ch: word1) {
+                ++freq[ch-'a'];
+            }
+            for (char ch: word2) {
+                --freq[ch-'a'];
+            }
+            return all_of(freq.begin(), freq.end(), [](int x) { return x == 0; });
+        };
+
+        for (int i = 1; i < n; ++i) {
+            if (compare(words[i], words[i-1])) {
+                continue;
+            }
+            res.push_back(words[i]);
+        }
+        return res;
+    }
+};
