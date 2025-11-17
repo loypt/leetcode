41697498b05e18658df0a9365f7298c38a468863
Loyio Hex <i@loyio.me>
2025-11-17 22:06:12 +0800

    add check if all 1s are at least length K places away

---
+++ N1001-2000/N1437-Check-If-All-1-s-Are-at-Least-Length-K-Places-Away/solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int n = nums.size();
+        int prev = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                if (prev != -1 && i - prev - 1 < k) {
+                    return false;
+                }
+                prev = i;
+            }
+        }
+        return true;
+    }
+};
