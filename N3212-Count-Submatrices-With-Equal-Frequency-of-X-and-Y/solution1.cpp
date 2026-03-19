e69ea2bd16686827cd5ff538c31d4a45a1696ce7
Loyio Hex <i@loyio.me>
2026-03-19 21:41:52 +0800

    add count submatrices with equal frequency of X and Y

---
+++ N3212-Count-Submatrices-With-Equal-Frequency-of-X-and-Y/solution1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        int ans = 0;
+        vector<vector<array<int, 2>>> sum(n + 1, vector<array<int, 2>>(m + 1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'X') {
+                    sum[i + 1][j + 1][0] =
+                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] + 1;
+                    sum[i + 1][j + 1][1] = 1;
+                } else if (grid[i][j] == 'Y') {
+                    sum[i + 1][j + 1][0] =
+                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0] - 1;
+                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
+                } else {
+                    sum[i + 1][j + 1][0] =
+                        sum[i + 1][j][0] + sum[i][j + 1][0] - sum[i][j][0];
+                    sum[i + 1][j + 1][1] = sum[i + 1][j][1] | sum[i][j + 1][1];
+                }
+                ans += (!sum[i + 1][j + 1][0] && sum[i + 1][j + 1][1]) ? 1 : 0;
+            }
+        }
+        return ans;
+    }
+};
