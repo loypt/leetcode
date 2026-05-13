99fd7979531b3c92dc2990d6981536a9874566a6
Loyio Hex <i@loyio.me>
2026-05-14 00:25:07 +0800

    add minimum moves to make array complementary

---
+++ N1674-Minimum-Moves-to-Make-Array-Complementary/solution1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<int> diff(2 * limit + 2, 0);
+
+        for (int i = 0; i < n / 2; ++i) {
+            int a = min(nums[i], nums[n - 1 - i]);
+            int b = max(nums[i], nums[n - 1 - i]);
+
+            diff[2] += 2;
+            diff[a + 1] -= 1;
+            diff[a + b] -= 1;
+            diff[a + b + 1] += 1;
+            diff[b + limit + 1] += 1;
+        }
+
+        int min_ops = n;
+        int current_ops = 0;
+
+        for (int c = 2; c <= 2 * limit; ++c) {
+            current_ops += diff[c];
+            min_ops = min(min_ops, current_ops);
+        }
+
+        return min_ops;
+    }
+};
