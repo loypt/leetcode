176d0e7c702f3b7315b7a6eb6c4215cb6bf9aec5
Loyio Hex <i@loyio.me>
2026-06-23 21:47:08 +0800

    add maximum number of balloons (#1264)

---
+++ N1189-Maximum-Number-of-Balloons/solution1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> cnt(5);
+        for (auto & ch : text) {
+            if (ch == 'b') {
+                cnt[0]++;
+            } else if (ch == 'a') {
+                cnt[1]++;
+            } else if (ch == 'l') {
+                cnt[2]++;
+            } else if (ch == 'o') {
+                cnt[3]++;
+            } else if (ch == 'n') {
+                cnt[4]++;
+            }
+        }
+        cnt[2] /= 2;
+        cnt[3] /= 2;
+        return *min_element(cnt.begin(), cnt.end());
+    }
+};
