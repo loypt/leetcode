655607e18c77a79ecca0274bca5b1b726615f469
Loyio Hex <i@loyio.me>
2026-06-30 22:12:04 +0800

    add number of strings that appear as substrings in word (#1268)

---
+++ N1967-Number-of-Strings-That-Appear-as-Substrings-in-Word/solution1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        auto check = [](const string& pattern, const string& word) -> bool{
+            int m = pattern.size();
+            int n = word.size();
+            vector<int> pi(m);
+            for (int i = 1, j = 0; i < m; i++){
+                while (j > 0 && pattern[i] != pattern[j]){
+                    j = pi[j - 1];
+                }
+                if (pattern[i] == pattern[j]){
+                    ++j;
+                }
+                pi[i] = j;
+            }
+            for (int i = 0, j = 0; i < n; i++){
+                while (j > 0 && word[i] != pattern[j]){
+                    j = pi[j - 1];
+                }
+                if (word[i] == pattern[j]){
+                    ++j;
+                }
+                if (j == m){
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        int res = 0;
+        for (const string& pattern : patterns){
+            res += check(pattern, word);
+        }
+        return res;
+    }
+};
