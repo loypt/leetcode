368375b45c2178967b8be3a532cf8928c90509a0
Loyio Hex <i@loyio.me>
2026-08-03 22:44:56 +0800

    add stone game (#1291)

---
+++ N0877-Stone-Game/solution1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int length = piles.size();
+        auto dp = vector<vector<int>>(length, vector<int>(length));
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] > 0;
+    }
+};
